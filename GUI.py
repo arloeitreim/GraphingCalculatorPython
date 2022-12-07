54f81100af1f4b611833778e142d07b86121e298
arloeitreim <105323402+arloeitreim@users.noreply.github.com>
2022-12-07 12:45:43 -0800

    Update GUI.py
    features:
    - add_sqrt(): adds "√ <span> _ </span>" to count.
    
    <span> and </span> dont show up in the GUI but are converted to parentheses during equals_method()
    
    translate_for_calculator(): translates count into something the calculator can parse. Contains no new code. made for organization/readability
    
    find_replace(): deletes the end tag of an html property if the start tag is deleted. Contains no new code, made for organization/readability.
    
    -find_distance: finds the distance that delete() has to delete. Contains no new code, made for organization/readability.

--- GUI.py
+++ GUI.py
@@ -46,7 +46,7 @@ class GUI(Ui_MainWindow):
         self.multiplication.clicked.connect(lambda: self.add(" * "))
         self.division.clicked.connect(lambda: self.add(" ÷ "))
         self.power.clicked.connect(lambda: self.add_exponent())
-        self.root.clicked.connect(lambda: self.add("√ "))
+        self.root.clicked.connect(lambda: self.add_sqrt())
         self.log.clicked.connect(lambda: self.add_log())
         self.negative.clicked.connect(lambda: self.add("-"))
         self.left_parenth.clicked.connect(lambda: self.add(" ( "))
@@ -55,13 +55,13 @@ class GUI(Ui_MainWindow):
         self.right_arrow.clicked.connect(lambda: self.arrows('right'))
         self.clear.clicked.connect(lambda: self.clear_method())
         self.equals_button.clicked.connect(lambda: self.equals_method())
-        #self.x.clicked.connect(lambda: self.add("x"))
+        #self.x.clicked.connect(lambda: self.add(' <span> 2 </span> '))
         self.answr.clicked.connect(lambda: self.add(self.previous))
 
 
     def clear_method(self):
         self.count = '_'
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
 
 
@@ -101,53 +101,90 @@ class GUI(Ui_MainWindow):
             #if self.count[underscore_index: underscore_index + 6] == ('</sup>' or '</sub>'):
                 distance = 6
         self.count = self.count[:underscore_index - distance*direction_int] + '_' + self.count[underscore_index - distance*direction_int:]
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
 
 
 
     def equals_method(self):
-        self.count = self.count.replace('_', '')
-        self.count = self.count.replace('<sub>', '')
-        self.count = self.count.replace('<sup>', '^')
-        self.count = self.count.replace('</sub>', '')
-        self.count = self.count.replace('</sup>', '')
-
+        self.translate_for_calculator()
         calc = Calculator()
         self.count = calc.PEMDAS(self.count) + '_'
         #self.count = self.count.replace("")
         self.previous = self.count
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
+        print(self.count)
+
+
+    def translate_for_calculator(self):
+        self.count = self.count.replace('_', '')
+        self.count = self.count.replace('<sub>', '(')
+        self.count = self.count.replace('<sup>', '^ (')
+        self.count = self.count.replace('<span>', '(')
+        self.count = self.count.replace('</sub>', ')')
+        self.count = self.count.replace('</sup>', ')')
+        self.count = self.count.replace('</span>', ')')
 
     def add_log(self):
         self.count = self.count.replace("_", ' log <sub> _ </sub>')
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
 
     def add_exponent(self):
         self.count = self.count.replace("_", ' <sup> _ </sup>')
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
+        self.equals.adjustSize()
+
+
+    def add_sqrt(self):
+        self.count = self.count.replace('_', ' √ <span> _ </span> ')
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
 
     def delete(self):
         underscore_index = self.count.index('_')
-        if self.count[underscore_index - 3: underscore_index] == 'log':
-            self.count = self.count[:underscore_index - 3] + self.count[underscore_index:]
-        elif (lambda x: x == '<sup>' or x == '<sub>')(self.count[underscore_index - 5: underscore_index]):
-            self.count = self.count[:underscore_index - 5] + self.count[underscore_index:]
-        elif (lambda x: x == '</sup>' or x == '</sub>')(self.count[underscore_index - 6: underscore_index]):
-            self.count = self.count[:underscore_index - 6] + self.count[underscore_index:]
-        else:
-            self.count = self.count[:underscore_index - 1] + self.count[underscore_index:]
-        self.equals.setText(self.count)
+        distance = self.find_distance(underscore_index)
+        if not underscore_index - distance < 0:
+            self.count = self.count[:underscore_index - distance] + self.count[underscore_index:]
+        #replace = self.find_replace()
+        #self.count = self.count.replace(replace, '')
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
+
+    def find_replace(self):
+        replace = ''
+        if (not '<sup>' in self.count) and ('</sup>' in self.count):
+            replace = '</sup>'
+        elif (not '<sub>' in self.count) and ('</sub>' in self.count):
+            replace = '</sub>'
+        elif ( not '<span>' in self.count) and ('</span>' in self.count):
+            replace = '</span>'
+        return replace
+
+
+    def find_distance(self, underscore_index):
+        distance: int
+        if self.count[underscore_index - 3: underscore_index] == 'log':
+            distance = 3
+        elif (lambda x: x == '<sup>' or x == '<sub>')(self.count[underscore_index - 5: underscore_index]):
+            distance = 5
+        elif (lambda x: x == '</sup>' or x == '</sub>' or x == '<span>')(self.count[underscore_index - 6: underscore_index]):
+            distance = 6
+        elif self.count[underscore_index - 7: underscore_index] == '</span>':
+            distance = 7
+        elif ' ' in self.count[underscore_index - 1]:
+            distance = 2
+        else:
+            distance = 1
+        return distance
+
 
 
     # text: text that will be added to count and displayed in the GUI
     def add(self, text):
         self.count = self.count.replace("_", text + "_")
-        self.equals.setText(self.count)
+        self.equals.setText(self.count.replace('  ', ' '))
         self.equals.adjustSize()
 
 if __name__ == "__main__":
